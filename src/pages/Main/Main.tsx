fd5c925fc66a4f903fad649b73955f815bf10603
GStarP <1700066935@qq.com>
2022-05-11 15:51:22 +0800

    feat(right-click-menu): :white_check_mark:

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -3,6 +3,7 @@ import { Box } from '@mui/system'
 import { Route, Routes } from 'react-router-dom'
 import Dashboard from './Dashboard/Dashboard'
 import TripleClickEvent from './TripleClickEvent/TripleClickEvent'
+import RightClickMenu from './RightClickMenu/RightClickMenu'
 
 function Main() {
   return (
@@ -16,6 +17,7 @@ function Main() {
       <Routes>
         <Route index element={<Dashboard />} />
         <Route path="triple-click-event" element={<TripleClickEvent />} />
+        <Route path="right-click-menu" element={<RightClickMenu />} />
       </Routes>
     </Box>
   )
